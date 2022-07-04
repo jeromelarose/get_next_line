326e2c4d24c38ca6c066522d853b5797de83c54d
jelarose <jelarose@student.42.fr>
2022-07-04 14:24:25 +0200

    Ajout de get_next_line

---
+++ get_next_line_bonus.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jelarose <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/06 19:05:17 by jelarose          #+#    #+#             */
+/*   Updated: 2019/12/06 19:05:22 by jelarose         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static int	read_list(t_stock st[255], int fd, char **line)
+{
+	char	*tmp;
+	char	*org;
+	int		i;
+
+	i = 0;
+	tmp = st[fd].str;
+	org = tmp;
+	if (!(*line = malloc(sizeof(char*) * (ft_strlen_set(tmp, '\n') + 1))))
+		return (-1);
+	while (*tmp && *tmp != '\n')
+		(*line)[i++] = *tmp++;
+	(*line)[i] = '\0';
+	if (*tmp == '\n')
+	{
+		st[fd].str = ft_strdup(++tmp);
+		free(org);
+		org = NULL;
+		return (1);
+	}
+	free(st[fd].str);
+	st[fd].str = NULL;
+	return (0);
+}
+
+static int	read_fd(int fd, t_stock st[255], char **line)
+{
+	int		rd;
+	char	*buff;
+
+	rd = 0;
+	if (!(buff = malloc(sizeof(*buff) * BUFFER_SIZE)))
+		return (-1);
+	while (!ft_strchr(st[fd].str, '\n') &&
+	((rd = read(fd, buff, BUFFER_SIZE)) > 0))
+		if (!(st[fd].str = ft_strcat_mal(st[fd].str, buff, rd)))
+			return (-1);
+	free(buff);
+	buff = NULL;
+	if (rd == -1)
+		return (-1);
+	if (!st[fd].str && rd == 0)
+	{
+		*line = ft_strdup("");
+		return (0);
+	}
+	return (read_list(st, fd, line));
+}
+
+int			get_next_line(int fd, char **line)
+{
+	static	t_stock st[255];
+
+	if (!line || fd < 0 || BUFFER_SIZE < 1)
+		return (-1);
+	return (read_fd(fd, st, line));
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jelarose <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/06 19:08:23 by jelarose          #+#    #+#             */
+/*   Updated: 2019/12/06 19:09:19 by jelarose         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+typedef struct	s_stock
+{
+	int			fd;
+	char		*str;
+}				t_stock;
+
+int				get_next_line(int fd, char **line);
+int				ft_strlen_set(char *str, char set);
+char			*ft_strdup(char *src);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strcat_mal(char *s1, char *s2, unsigned size);
+
+#endif
